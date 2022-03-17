49c0f04b12ef005f3140f4197924136a859fe04e
lazycoder <alazycoder101@gmail.com>
2022-03-17 00:16:09 +0000

    fix problem caused by 8.1
    https://php.watch/versions/8.1/PgSQL-resource
    
    Note that the is_resource function no longer returns true for the new class objects, and it may now be necessary to change the is_resource check with a comparison to false, or account for the new class instances as well.

--- wp-content/pg4wp/core.php
+++ wp-content/pg4wp/core.php
@@ -28,6 +28,7 @@ $replaces = array(
 	'mysql_'	=> 'wpsql_',
 	'<?php'		=> '',
 	'?>'		=> '',
+    'is_resource( $this->dbh )' => '(is_resource($this->dbh) || $this->dbh instanceof \PgSql\Connection)'
 );
 // Ensure class uses the replaced mysql_ functions rather than mysqli_
 define( 'WP_USE_EXT_MYSQL', true);
